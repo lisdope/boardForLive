da4b6336b4b6be5f34d49b0b74233be714feda97
dkdlel4335 <dkdlel4335@gmail.com>
2021-04-09 13:10:43 +0900

    util commit

--- src/jdbc/connection/ConnectionProvider.java
+++ src/jdbc/connection/ConnectionProvider.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class ConnectionProvider{
 	
 	public static Connection getConnection() throws SQLException {
-		return DriverManager.getConnection("jdbc:apache:commons:dbcp:poolname"); // web.xml = poolname
+		return DriverManager.getConnection("jdbc:apache:commons:dbcp:live"); // web.xml = poolname
 	}
 
 }
